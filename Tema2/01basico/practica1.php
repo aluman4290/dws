d3fbba6cf0a8d6d55e6d11bf2d45c98f0c9eea0b
Jordi Manrqiue <aluman4290@ieselcaminas.org>
2022-10-01 08:41:08 +0200

    Create practica1.php

---
+++ Tema2/01basico/practica1.php
@@ -0,0 +1,23 @@
+<?php
+$title = 'Desarrollo web con PHP 7';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $title ?></title>
+</head>
+
+<body>
+    <?php
+    $texto = "Hola mundo";
+    echo ($texto);
+    ?>
+
+</body>
+
+</html>
