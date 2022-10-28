598c597f2bb1dd5b0d3eb54a398e746277d28524
Jordi Manrqiue <aluman4290@ieselcaminas.org>
2022-10-28 06:21:45 +0200

    Update Imagen.php

---
+++ Tema5/Imagen.php
@@ -0,0 +1,20 @@
+<?php
+
+class Imagen
+{
+
+    private $src;
+    private $border;
+    private $ruta_images = 'images/';
+
+    function __construct($src, $border = 0)
+    {
+        $this->src = $src;
+        $this->border = $border;
+    }
+
+    public function __toString()
+    {
+        return "<img src=" . $this->ruta_images . $this->src . " border=" . $this->border . "/>";
+    }
+}
