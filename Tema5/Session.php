ba40dc88723160a5a2587fb4df846ba163580c8c
Jordi Manrqiue <aluman4290@ieselcaminas.org>
2022-10-28 05:40:22 +0200

    Update Session.php

--- Tema5/Session.php
+++ Tema5/Session.php
@@ -40,14 +40,4 @@ class Session
     }
 }
 
-$session = new Session();
 
-$session->setAttribute("nombre", "Fernando");
-
-echo "Valor de la propiedad nombre: " . $session->getAttribute("nombre") . "<br/>";
-
-$session->deleteAttribute("nombre");
-
-echo "Valor de la propiedad nombre: " . $session->getAttribute("nombre") . "<br/>";
-
-$session->destroySession();
