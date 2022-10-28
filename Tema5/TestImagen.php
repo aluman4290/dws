7aea03aeadb68656ab3c17ab467ebae35f00ead7
Jordi Manrqiue <aluman4290@ieselcaminas.org>
2022-10-28 06:37:05 +0200

    Prueba atributos en protected

--- Tema5/TestImagen.php
+++ Tema5/TestImagen.php
@@ -15,8 +15,16 @@
     $img = new Imagen('img1.jfif');
     $img2 = new Imagen("img2.jfif", 10);
 
+    echo "<h1>Test 1</h1>";
     echo $img->__toString();
     echo $img2->__toString();
+    echo "</br>";
+
+    echo "<h1>Test 2</h1>";
+    $img2->src = 'img1.jfif';
+    echo $img2->__toString();
+    echo "</br>";
+
     ?>
 </body>
 
