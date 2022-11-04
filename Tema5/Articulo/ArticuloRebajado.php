43a02d54b23963f1286411340884fda6dbcb062f
Jordi Manrqiue <aluman4290@ieselcaminas.org>
2022-11-04 05:39:19 +0100

    Create ArticuloRebajado.php

---
+++ Tema5/Articulo/ArticuloRebajado.php
@@ -0,0 +1,29 @@
+<?php
+final class  ArticuloRebajado extends Articulo
+{
+    private $rebaja  = 0;
+    private $articulo;
+
+    function __construct($pNombre, $pPrecio, $pRebaja)
+    {
+        $this->articulo  = new Articulo($pNombre, $pPrecio);
+        $this->rebaja = $pRebaja;
+    }
+
+    private function calculaDescuento()
+    {
+        return $this->articulo->precio * $this->rebaja / 100;
+    }
+
+    public function precioRebajado()
+    {
+        return $this->precio - $this->calculaDescuento();
+    }
+
+    public function __toString()
+    {
+        return $this->articulo->__toString() . '</br>'
+            . 'La rebaja es: ' . $this->rebaja . ' %' . '</br>'
+            . 'El descuento es ' . self::calculaDescuento() . '€';
+    }
+}
