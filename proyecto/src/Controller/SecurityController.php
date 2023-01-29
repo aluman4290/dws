acef15b0ebc3a3ac8fa90b6b525dc09aa0cfad43
Jordi Manrqiue <aluman4290@ieselcaminas.org>
2023-01-29 10:21:03 +0100

    Add logging out

---
+++ proyecto/src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/security', name: 'app_security')]
+    public function index(): Response
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+}
+
+namespace App\Controller\SecurityController;
+
+use Symfony\Bundle\SecurityBundle\Security;
+
+class SecurityController
+{
+    public function someAction(Security $security): Response
+    {
+        // logout the user in on the current firewall
+        $response = $security->logout();
+
+        // you can also disable the csrf logout
+        $response = $security->logout(false);
+
+        // ... return $response (if set) or e.g. redirect to the homepage
+    }
+}
